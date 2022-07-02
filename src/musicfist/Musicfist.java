0d45149d8088df80b857c51b5b31cd09ac367e06
drexploit01 <ashiqmuhammedep@gmail.com>
2022-07-02 18:35:29 +0000

    first commit

---
+++ src/musicfist/Musicfist.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package musicfist;
+
+/**
+ *
+ * @author kali
+ */
+public class Musicfist {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        
+
+firstpage sd = new firstpage();
+sd.setVisible(true);
+    }
+    
+}
